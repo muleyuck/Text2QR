6dbe022c3cb3efbaf43778259e7c1a84cb9b94ef
muleyuck <takuty.008.awenite.1121@gmail.com>
2026-02-01 10:06:52 +0900

    fix: remove usage of noNonNullAssertion at main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,12 @@ import { createRoot } from "react-dom/client"
 import "@/index.css"
 import App from "@/App.tsx"
 
-// biome-ignore lint/style/noNonNullAssertion: ignore
-createRoot(document.getElementById("root")!).render(
+const rootElement = document.getElementById("root")
+if (!rootElement) {
+  throw new Error("Root element not found")
+}
+
+createRoot(rootElement).render(
   <StrictMode>
     <App />
   </StrictMode>,
