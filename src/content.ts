7b2423da8c5b010c32e5ed033a7489567fc2840c
muleyuck <takuty.008.awenite.1121@gmail.com>
2026-02-01 11:41:08 +0900

    feat: support generating QR code by clicking extension icon

--- src/content.ts
+++ src/content.ts
@@ -1,1 +1,9 @@
-console.log("Text to QR Code extension loaded")
+import { CHROME_MESSAGE_TYPE } from "@/constant"
+
+chrome.runtime.onMessage.addListener((request, _sender, sendResponse) => {
+  if (request.type === CHROME_MESSAGE_TYPE) {
+    const selectedText = window.getSelection()?.toString() || ""
+    sendResponse({ text: selectedText })
+  }
+  return true
+})
